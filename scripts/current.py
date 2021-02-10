a5be1ffd4b14007c2ffcf4c92b8bfcecd36950e4
Marc Garner <m@marcgarner.com>
2021-02-10 08:56:35 -0700

    Update current.py

--- scripts/current.py
+++ scripts/current.py
@@ -15,7 +15,7 @@ ads = ADS.ADS1115(i2c)
 
 # Create single-ended input on channel 0
 a0 = AnalogIn(ads, ADS.P0)
-# chan2 = AnalogIn(ads, ADS.P1)
+a1 = AnalogIn(ads, ADS.P1)
 # Create differential input between channel 0 and 1
 # chan = AnalogIn(ads, ADS.P0, ADS.P1)
 
@@ -23,6 +23,6 @@ print("{:>5}\t{:>5}".format("raw", "v"))
 
 while True:
     # print("{:>5}\t{:>5.3f}".format(chan.value, chan.voltage))
-    # print("chan2 = ", chan2.value, chan2.voltage)
+    print("a1 = ", a1.value, a1.voltage)
     print("a0 = ", a0.value, a0.voltage)
     time.sleep(0.5)
