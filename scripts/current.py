777b0e6e84ac4faf514bebd21a51cc0646d3fed7
Marc Garner <m@marcgarner.com>
2021-02-10 15:41:30 -0700

    Update current.py

--- scripts/current.py
+++ scripts/current.py
@@ -26,22 +26,14 @@ print("{:>5}\t{:>5}".format("raw", "v"))
 while True:
     # print("{:>5}\t{:>5.3f}".format(chan.value, chan.voltage))
     # print("a1 = ", a1.value, a1.voltage)
-    GAIN = 1
-	state=False
+ 
 
     # Read all the ADC channel values in a list.
-    values = [0]*50
-    for i in range(50):
-        #values[i] = adc.read_adc(0, gain=GAIN)
-        values[i] = a0.value-20848
-    
+    values = a0.value-20848
     if max(values)>0:
-        print("Device is On")
-        print(max(values))
-        state=True
+    	print("Device is On", max(values))
     else:
-        print("Device is OFF")
-        print(max(values))
-        state=False
-    print("a0 = ", a0.value, a0.voltage)
+        print("Device is OFF", max(values))
+        
+    # print("a0 = ", a0.value, a0.voltage)
     time.sleep(0.5)
