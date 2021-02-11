d01340073f201d04cce88b05dae69f1061db93eb
Marc Garner <m@marcgarner.com>
2021-02-10 17:40:10 -0700

    Update current.py

--- scripts/current.py
+++ scripts/current.py
@@ -60,7 +60,7 @@ values = [0]*100
 while True:
     for i in range(100):
         #values[i] = adc.read_adc(0, gain=GAIN)   print(math.ceil(4.2))
-        values[i] = a0.voltage - 2.6080795922727136
+        values[i] = a0.voltage - 2.606079531235694
 
     print(max(values))
 
