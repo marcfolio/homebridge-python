8224d603a2a1020886a95179e68e83dc197a918c
Marc Garner <m@marcgarner.com>
2021-02-10 18:24:21 -0700

    Update current.py

--- scripts/current.py
+++ scripts/current.py
@@ -75,15 +75,15 @@ values = [0]*100
 while True:
     for i in range(100):
         #values[i] = adc.read_adc(0, gain=GAIN)   print(math.ceil(4.2))
-        values[i] = a0.value -20848
+        values[i] = a1.value -20848
 
     # print(max(values))
 
     if max(values)>0:
-    	print("Divice is true")
+    	print("Divice is false")
     	print(max(values))
     else:
-    	print("Divice is false")
+    	print("Divice is true")
     	print(max(values))
 
     # print("a1 = ", a1.value, a1.voltage)
