fc561fe78b4b42d7928ec02851d342d4d7de69ff
Marc Garner <m@marcgarner.com>
2021-02-12 14:32:54 -0700

    Update current.py

--- scripts/current.py
+++ scripts/current.py
@@ -12,7 +12,7 @@ ads = ADS.ADS1115(i2c)
 a0 = AnalogIn(ads, ADS.P0)
 a1 = AnalogIn(ads, ADS.P1)
 
-maxnum = 20864
+print(a0.value, a0.voltage)
 
 
 
@@ -69,50 +69,50 @@ maxnum = 20864
 #     	print("Divice is false")
 #     	print(max(values))
 
-a0values = [0]*100
-a1values = [0]*100
-while True:
-    for i in range(100):
-        #values[i] = adc.read_adc(0, gain=GAIN)   print(math.ceil(4.2))
-        a0values[i] = a0.value - 20848
-        a1values[i] = a1.value -20848
+# a0values = [0]*100
+# a1values = [0]*100
+# while True:
+#     for i in range(100):
+#         #values[i] = adc.read_adc(0, gain=GAIN)   print(math.ceil(4.2))
+#         a0values[i] = a0.value - 20848
+#         a1values[i] = a1.value -20848
 
-    # print(max(values))
+#     # print(max(values))
 
-    # a0 exterior light off && interior light off
-    if max(a0values) >= 16 and max(a0values) <= 0:
-    	print("ext OFF", max(a0values), a0.voltage)
-    elif max(a0values) >= 240 and max(a0values) <= 272:
-    	print("ext OFF", max(a0values), a0.voltage)
-    elif max(a0values) == -64:
-    	print("ext ON", max(a0values), a0.voltage)
-    elif max(a0values) <= 192 and max(a0values) >= 176:
-    	print("ext ON", max(a0values), a0.voltage)
+#     # a0 exterior light off && interior light off
+#     if max(a0values) >= 16 and max(a0values) <= 0:
+#     	print("ext OFF", max(a0values), a0.voltage)
+#     elif max(a0values) >= 240 and max(a0values) <= 272:
+#     	print("ext OFF", max(a0values), a0.voltage)
+#     elif max(a0values) == -64:
+#     	print("ext ON", max(a0values), a0.voltage)
+#     elif max(a0values) <= 192 and max(a0values) >= 176:
+#     	print("ext ON", max(a0values), a0.voltage)
 
-	# if max(a1values) >= 16 and max(a1values) <= 0:
- #    	print("ext OFF", max(a1values), a1.voltage)
- #    elif max(a1values) >= 240 and max(a1values) <= 272:
- #    	print("ext OFF", max(a1values), a1.voltage)
- #    elif max(a1values) == -64:
- #    	print("ext ON", max(a1values), a1.voltage)
- #    elif max(a0vlues) <= 192 and max(a1values) >= 176:
- #    	print("ext ON", max(a1values), a1.voltage)    	
+# 	# if max(a1values) >= 16 and max(a1values) <= 0:
+#  #    	print("ext OFF", max(a1values), a1.voltage)
+#  #    elif max(a1values) >= 240 and max(a1values) <= 272:
+#  #    	print("ext OFF", max(a1values), a1.voltage)
+#  #    elif max(a1values) == -64:
+#  #    	print("ext ON", max(a1values), a1.voltage)
+#  #    elif max(a0vlues) <= 192 and max(a1values) >= 176:
+#  #    	print("ext ON", max(a1values), a1.voltage)    	
 
-    # if max(a1values)>=0:
-    # 	print("interior light is off", max(a1values), a1.voltage)
-    # else:
-    # 	print("interior light is on", max(a1values), a1.voltage)
+#     # if max(a1values)>=0:
+#     # 	print("interior light is off", max(a1values), a1.voltage)
+#     # else:
+#     # 	print("interior light is on", max(a1values), a1.voltage)
 
 
-    # if max(a0values)>=0:
-    # 	print("exterior light is off", max(a0values), a0.voltage)
-    # else:
-    # 	print("exterior light is on", max(a0values), a0.voltage)
+#     # if max(a0values)>=0:
+#     # 	print("exterior light is off", max(a0values), a0.voltage)
+#     # else:
+#     # 	print("exterior light is on", max(a0values), a0.voltage)
     	
 
 
-    # print("a1 = ", a1.value, a1.voltage)
+#     # print("a1 = ", a1.value, a1.voltage)
                     
-    # print("OFF")
-    # #print(math.ceil(sum(values) / len(values)))
-    # print(max(values))  
+#     # print("OFF")
+#     # #print(math.ceil(sum(values) / len(values)))
+#     # print(max(values))  
