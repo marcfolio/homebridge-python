5a17d1f109f16b5191dfd2eddc44edc441b8065b
Marc Garner <m@marcgarner.com>
2021-02-12 14:30:11 -0700

    Update current.py

--- scripts/current.py
+++ scripts/current.py
@@ -1,16 +1,11 @@
 # SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
 # SPDX-License-Identifier: MIT
-
 import time
 import board
 import busio
+i2c = busio.I2C(board.SCL, board.SDA)
 import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
-
-# Create the I2C bus
-i2c = busio.I2C(board.SCL, board.SDA)
-
-# Create the ADC object using the I2C bus
 ads = ADS.ADS1115(i2c)
 
 # Create single-ended input on channel 0
@@ -18,6 +13,8 @@ a0 = AnalogIn(ads, ADS.P0)
 a1 = AnalogIn(ads, ADS.P1)
 
 maxnum = 20864
+
+
 
 # Create differential input between channel 0 and 1
 # chan = AnalogIn(ads, ADS.P0, ADS.P1)
@@ -80,17 +77,17 @@ while True:
         a0values[i] = a0.value - 20848
         a1values[i] = a1.value -20848
 
-    # # print(max(values))
+    # print(max(values))
 
-    # # a0 exterior light off && interior light off
-    # if max(a0values) >= 16 and max(a0values) <= 0:
-    # 	print("ext OFF", max(a0values), a0.voltage)
-    # elif max(a0values) >= 240 and max(a0values) <= 272:
-    # 	print("ext OFF", max(a0values), a0.voltage)
-    # elif max(a0values) == -64:
-    # 	print("ext ON", max(a0values), a0.voltage)
-    # elif max(a0values) <= 192 and max(a0values) >= 176:
-    # 	print("ext ON", max(a0values), a0.voltage)
+    # a0 exterior light off && interior light off
+    if max(a0values) >= 16 and max(a0values) <= 0:
+    	print("ext OFF", max(a0values), a0.voltage)
+    elif max(a0values) >= 240 and max(a0values) <= 272:
+    	print("ext OFF", max(a0values), a0.voltage)
+    elif max(a0values) == -64:
+    	print("ext ON", max(a0values), a0.voltage)
+    elif max(a0values) <= 192 and max(a0values) >= 176:
+    	print("ext ON", max(a0values), a0.voltage)
 
 	# if max(a1values) >= 16 and max(a1values) <= 0:
  #    	print("ext OFF", max(a1values), a1.voltage)
