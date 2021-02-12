41f008d44d9488954aa12325207123420ab813a6
Marc Garner <m@marcgarner.com>
2021-02-12 14:37:13 -0700

    Update c.py

--- scripts/c.py
+++ scripts/c.py
@@ -12,4 +12,30 @@ ads = ADS.ADS1115(i2c)
 a0 = AnalogIn(ads, ADS.P0)
 a1 = AnalogIn(ads, ADS.P1)
 
-print(a0.value, a0.voltage)
+
+
+a0values = [0]*100
+a1values = [0]*100
+while True:
+    for i in range(100):
+        a0values[i] = a0.value - 20848
+        a1values[i] = a1.value -20848
+
+    # a0 exterior light off && interior light off
+    if max(a0values) >= 16 and max(a0values) <= 0:
+    	print("ext OFF", max(a0values), a0.voltage)
+    elif max(a0values) >= 240 and max(a0values) <= 272:
+    	print("ext OFF", max(a0values), a0.voltage)
+    elif max(a0values) == -64:
+    	print("ext ON", max(a0values), a0.voltage)
+    elif max(a0values) <= 192 and max(a0values) >= 176:
+    	print("ext ON", max(a0values), a0.voltage)
+
+	# if max(a1values) >= 16 and max(a1values) <= 0:
+ #    	print("ext OFF", max(a1values), a1.voltage)
+ #    elif max(a1values) >= 240 and max(a1values) <= 272:
+ #    	print("ext OFF", max(a1values), a1.voltage)
+ #    elif max(a1values) == -64:
+ #    	print("ext ON", max(a1values), a1.voltage)
+ #    elif max(a0vlues) <= 192 and max(a1values) >= 176:
+ #    	print("ext ON", max(a1values), a1.voltage)    
