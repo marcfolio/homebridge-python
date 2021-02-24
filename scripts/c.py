7d897c4836e3b150c6d1242950bc98e492f722d0
Marc Garner <m@marcgarner.com>
2021-02-23 17:27:09 -0700

    Update c.py

--- scripts/c.py
+++ scripts/c.py
@@ -23,7 +23,7 @@ for i in range(100):
 	a0values[i] = a0.value - 20672
 	a1values[i] = a1.value - 20672
 
-	print("a0: ", a0values[i], max(a0values))
+	print("a0: ", max(a0values))
 	# print("a1: ", a1values[i], max(a1values))
 
 	# # INT OFF / EXT OFF
